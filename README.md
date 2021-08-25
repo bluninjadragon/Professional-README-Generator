6ca82f4cf253c9839393c4fa5abfda61bf2ef798
bluninjadragon <bluninjadragon@gmail.com>
2021-08-24 23:53:54 -0500

    edited starting template. added the README markup, added each item in the read me as an object, and npm installed inquirer

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# Professional-README-Generator
+# a
+## Description
+a.
+Link to the Github repository:a
+Deployed application a
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Contact Information](#contact)
+## Installation
+a.
+## Usage
+a.
+## License
+MIT
+## Contributing
+false
+## Tests
+a
+## Contact
+GitHub profile: https://github.com/a
+Email: a
--- index.js
+++ index.js
@@ -1,74 +1,105 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
-// create writeFile function using promises instead of a callback function
-const writeFileAsync = util.promisify(fs.writeFile);
+//README format that you are returning
+const generateREADME = (answers) =>
+  `# ${answers.title}
+## Description
+${answers.description}.
+Link to the Github repository:${answers.repo}
+Deployed application ${answers.deployed}
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Contact Information](#contact)
+## Installation
+${answers.install}.
+## Usage
+${answers.usage}.
+## License
+${answers.license}
+## Contributing
+${answers.contribute}
+## Tests
+${answers.test}
+## Contact
+GitHub profile: https://github.com/${answers.github}
+Email: ${answers.email}
+`;
 
 const promptUser = () => {
   return inquirer.prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      type: "input",
+      name: "title",
+      message: "Project Title:",
     },
     {
-      type: 'input',
-      name: 'location',
-      message: 'Where are you from?',
+      type: "input",
+      name: "description",
+      message: "Summarize what your project does",
     },
     {
-      type: 'input',
-      name: 'hobby',
-      message: 'What is your favorite hobby?',
+      type: "input",
+      name: "install",
+      message: "Installation instructions, packages, or command lines used:",
     },
     {
-      type: 'input',
-      name: 'food',
-      message: 'What is your favorite food?',
+      type: "input",
+      name: "usage",
+      message: "Describe how this is used:",
     },
     {
-      type: 'input',
-      name: 'github',
-      message: 'Enter your GitHub Username',
+      type: "input",
+      name: "repo",
+      message: "Link to Github repository:",
     },
     {
-      type: 'input',
-      name: 'linkedin',
-      message: 'Enter your LinkedIn URL.',
+      type: "input",
+      name: "deployed",
+      message: "Deployed Link:",
+    },
+    {
+      type: "confirm",
+      name: "contribute",
+      message: "Allow others to contribute?",
+    },
+    {
+      type: "list",
+      name: "license",
+      message: "License:",
+      choices: ["MIT", "GNU GPLv3", "Apache License 2.0", "ISC"],
+    },
+    {
+      type: "input",
+      name: "test",
+      message: "Test files if applicable:",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "GitHub username:",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "Email contact:",
     },
   ]);
 };
 
-const generateHTML = (answers) =>
-  `<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-  <title>Document</title>
-</head>
-<body>
-  <div class="jumbotron jumbotron-fluid">
-  <div class="container">
-    <h1 class="display-4">Hi! My name is ${answers.name}</h1>
-    <p class="lead">I am from ${answers.location}.</p>
-    <h3>Example heading <span class="badge badge-secondary">Contact Me</span></h3>
-    <ul class="list-group">
-      <li class="list-group-item">My GitHub username is ${answers.github}</li>
-      <li class="list-group-item">LinkedIn: ${answers.linkedin}</li>
-    </ul>
-  </div>
-</div>
-</body>
-</html>`;
+// create writeFile function using promises instead of a callback function
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // Bonus using writeFileAsync as a promise
 const init = () => {
   promptUser()
-    .then((answers) => writeFileAsync('index.html', generateHTML(answers)))
-    .then(() => console.log('Successfully wrote to index.html'))
+    .then((answers) => writeFileAsync("README.md", generateREADME(answers)))
+    .then(() => console.log("Successfully wrote to index.html"))
     .catch((err) => console.error(err));
 };
 
